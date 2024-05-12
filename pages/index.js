0e31e22c99acb65108559e7c64a0e5fac018dec0
RodrigoGondim <111583608+RodrigoGondim@users.noreply.github.com>
2024-05-12 23:23:20 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,4 @@
 function Home() {
   return <h1>Página Inicial teste deploy </h1>;
 }
-
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
